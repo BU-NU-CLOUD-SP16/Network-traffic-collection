d839773e4959c2fde2699a8d121ac2636240864b
akash kakumani <akashkakumani@gmail.com>
2016-03-01 12:01:51 -0500

    change page after creating group to home page

--- dbScriptBatched.py
+++ dbScriptBatched.py
@@ -1,12 +1,19 @@
 __author__ = 'akashkakumani'
 
+
 import time
 import datetime
 from influxdb import InfluxDBClient
 
+
+
+
 def createCommand(sip, dip, domain, url, user_agent, referrer, code, action, bytes, type):
     jsonArray = []
+
+
     for i in range(0,1000):
+
         json_body = {
                 "measurement": "http",
                 "tags": {
@@ -27,20 +34,20 @@ def createCommand(sip, dip, domain, url, user_agent, referrer, code, action, byt
                 "value": 12.0
             }
             }
+
         jsonArray.append(json_body)
     return jsonArray
 
 
 
 client = InfluxDBClient(host='127.0.0.1', port=8086, database='newDB')
+
+v = createCommand("127.127.12.127", "4.4.4.4", "google.com", "http://google.com", "SomeAgent", "Somereferrer",
+       "404", "GET", "78", "html/plain")
+
 t0 = time.time()
 
-#n = 0
-#while(n<1000):
-client.write_points(createCommand("127.127.12.127", "4.4.4.4", "google.com", "http://google.com", "SomeAgent", "Somereferrer",
-       "404", "GET", "78", "html/plain"))
-#    n+=1
-#    print(n)
+client.write_points(v)
 
 t1 = time.time()
 
