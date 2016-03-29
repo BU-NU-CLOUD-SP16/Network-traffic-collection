404992a44d94710d4ca5c2b748d012999659e3ed
Ubuntu <akashkakumani@gmail.com>
2016-03-29 00:27:44 +0000

    modified:   dbScriptBatched.py

--- dbScriptBatched.py
+++ dbScriptBatched.py
@@ -1,18 +1,16 @@
 __author__ = 'akashkakumani'
 
+#from __future__ import with_statement
 
 import time
 import datetime
 from influxdb import InfluxDBClient
 
-
-
-
 def createCommand(sip, dip, domain, url, user_agent, referrer, code, action, bytes, type):
     jsonArray = []
 
 
-    for i in range(0,1000):
+    for i in range(0,1000000):
 
         json_body = {
                 "measurement": "http",
@@ -40,19 +38,49 @@ def createCommand(sip, dip, domain, url, user_agent, referrer, code, action, byt
 
 
 
-client = InfluxDBClient(host='127.0.0.1', port=8086, database='newDB')
+#client = InfluxDBClient(host='127.0.0.1', port=8086, database='newDB')
 
 v = createCommand("127.127.12.127", "4.4.4.4", "google.com", "http://google.com", "SomeAgent", "Somereferrer",
        "404", "GET", "78", "html/plain")
 
-t0 = time.time()
+#t0 = time.time()
+#client.write_points(v)
+#t1 = time.time()
+#influx = t1-t0
+#print("TIME INFLUX: {}".format(influx))
 
-client.write_points(v)
+with open('output.txt','a') as f:
+	t2 = time.time()
+	f.write(str(v))
+	t3 = time.time()
+	writeNormal = t3-t2
+	print("TIME WRITE: {}".format(writeNormal))
 
-t1 = time.time()
 
-total = t1-t0
+with open('RAM/output.txt','a') as f:
+	t4 = time.time()
+	f.write(str(v))
+	t5 = time.time()
+	writeRAM = t5-t4
+	print("TIME RAM WRITE: {}".format(writeRAM))
 
 
-print("TOTAL::::")
-print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
