4dde78a9993f35de62e74eccb9197fd4f2706323
ojanoudi <ojanoudi@bu.edu>
2016-04-26 00:49:41 -0400

    changed so that results are written to a log file

--- Performance-Testing/perf_test.py
+++ Performance-Testing/perf_test.py
@@ -1,7 +1,5 @@
 import sys
 from subprocess import Popen, PIPE, check_output, call
-import matplotlib.pyplot as plt
-import numpy as np
 
 if __name__ == '__main__':
     # PERFORMANCE TESTER FOR TRAFFIC COLLECTION / MALICIOUS TRAFFIC DETECTION
@@ -33,42 +31,8 @@ if __name__ == '__main__':
         p3 = check_output(cmd3, shell=True,stdin=PIPE)
         Rresults.append(float(p2r.replace("\n","")))
         Dresults.append(float(p3.replace("\n","")))
-
-    # TURN RESULTS INTO NUMPY ARRAYS
-    Rresults = np.float64(1.00) * Rresults
-    Dresults = np.float64(1.00) * Dresults
-
-    fig = plt.figure()
-
-    fig, ax = plt.subplots()
-    index = np.arange(len(Nlist))
-    bar_width = 0.35
-    opacity = 0.4
-    error_config = {'ecolor': '0.3'}
-    plt.gca().get_xaxis().get_major_formatter().set_useOffset(False)
-
-    rects1 = plt.bar(index, Rresults, bar_width,
-                     alpha=opacity,
-                     color='b',
-                     error_kw=error_config,
-                     label='Performance Test of Packet Collection')
-
-    rects2 = plt.bar(index + bar_width, Dresults, bar_width,
-                     alpha=opacity,
-                     color='r',
-                     error_kw=error_config,
-                     label='Performance Test of Packet Detection')
-
-    plt.xlabel('Number of Requests')
-    plt.ylabel('Latency (Time/Packet')
-    plt.title('Performance Testing')
-    plt.legend()
-    plt.tight_layout()
-    plt.xticks(index + bar_width, (Nlist))
-    plt.savefig('perf.png', dpi=300)
-
-
-
-
-
-
+    
+    for n in range(0, len(Nlist)):
+        N = Nlist[n]
+        with open("perf_log.txt", "a") as myfile:
+            myfile.write("READING LATENCY FOR {} REQUESTS: {} -- DETECTION LATENCY FOR {} REQUESTS: {}".format(N,Rresults[n],N,Dresults[n])
